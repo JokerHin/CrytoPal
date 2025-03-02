321fff1e957d98f7ec4faa11afb76d80fdf0c3b3
JokerHin <chokarhin@gmail.com>
2025-03-02 20:28:05 +0800

    Initialize subgraph

---
+++ cryptopal/cryptopalgraph/tests/unnamed-token-utils.ts
@@ -0,0 +1,33 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import { Deposit, Withdraw } from "../generated/Unnamed token/Unnamed token"
+
+export function createDepositEvent(user: Address, amount: BigInt): Deposit {
+  let depositEvent = changetype<Deposit>(newMockEvent())
+
+  depositEvent.parameters = new Array()
+
+  depositEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  depositEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return depositEvent
+}
+
+export function createWithdrawEvent(user: Address, amount: BigInt): Withdraw {
+  let withdrawEvent = changetype<Withdraw>(newMockEvent())
+
+  withdrawEvent.parameters = new Array()
+
+  withdrawEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+  withdrawEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return withdrawEvent
+}
